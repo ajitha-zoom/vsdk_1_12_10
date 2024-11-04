2e49458f091d81e82c942fb379d30775da43acaa
ajitha-zoom <123167421+ajitha-zoom@users.noreply.github.com>
2024-11-04 15:10:46 -0500

    Init

---
+++ src/index.tsx
@@ -0,0 +1,165 @@
+/* eslint-disable no-restricted-globals */
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import ZoomVideo from '@zoom/videosdk';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import ZoomContext from './context/zoom-context';
+import { devConfig } from './config/dev';
+import { b64DecodeUnicode, generateVideoToken } from './utils/util';
+
+let meetingArgs: any = Object.fromEntries(new URLSearchParams(location.search));
+// Add enforceGalleryView to turn on the gallery view without SharedAddayBuffer
+if (!meetingArgs.sdkKey || !meetingArgs.topic || !meetingArgs.name || !meetingArgs.signature) {
+  meetingArgs = { ...devConfig, ...meetingArgs };
+  meetingArgs.enforceGalleryView = true;
+}
+
+if (meetingArgs.web && meetingArgs.web !== '0') {
+  ['topic', 'name', 'password', 'sessionKey', 'userIdentity'].forEach((field) => {
+    if (Object.hasOwn(meetingArgs, field)) {
+      try {
+        meetingArgs[field] = b64DecodeUnicode(meetingArgs[field]);
+      } catch (e) {
+        console.log('ingore base64 decode', field, meetingArgs[field]);
+      }
+    }
+  });
+  if (meetingArgs.role) {
+    meetingArgs.role = parseInt(meetingArgs.role, 10);
+  } else {
+    meetingArgs.role = 1;
+  }
+}
+// enforce use <video> tag render video, https://marketplacefront.zoom.us/sdk/custom/web/modules/Stream.html#attachVideo
+meetingArgs.useVideoPlayer = 1;
+
+['enforceGalleryView', 'enforceVB', 'cloud_recording_option', 'cloud_recording_election'].forEach((field) => {
+  if (Object.hasOwn(meetingArgs, field)) {
+    try {
+      meetingArgs[field] = Number(meetingArgs[field]);
+    } catch (e) {
+      meetingArgs[field] = 0;
+    }
+  }
+});
+if (meetingArgs?.telemetry_tracking_id) {
+  try {
+    meetingArgs.telemetry_tracking_id = b64DecodeUnicode(meetingArgs.telemetry_tracking_id);
+  } catch (e) {}
+} else {
+  meetingArgs.telemetry_tracking_id = '';
+}
+
+/*if (!meetingArgs.signature && meetingArgs.sdkSecret && meetingArgs.topic) {
+  meetingArgs.signature = generateVideoToken(
+    meetingArgs.sdkKey,
+    meetingArgs.sdkSecret,
+    meetingArgs.topic,
+    meetingArgs.sessionKey,
+    meetingArgs.userIdentity,
+    Number(meetingArgs.role ?? 1),
+    meetingArgs.cloud_recording_option,
+    meetingArgs.cloud_recording_election,
+    meetingArgs.telemetry_tracking_id
+  );
+  console.log('=====================================');
+  console.log('meetingArgs', meetingArgs);
+  }*/
+
+let namefromPrompt = null;
+let sessionName = null;
+let passcode = null;
+
+while (namefromPrompt == null || namefromPrompt == '') {
+  namefromPrompt = prompt('Enter your name', '');
+}
+console.log(namefromPrompt);
+meetingArgs.name = namefromPrompt;
+
+while (sessionName == null || sessionName == '') {
+  sessionName = prompt('Enter session name', '');
+}
+console.log(sessionName);
+meetingArgs.sessionKey = sessionName;
+meetingArgs.topic = sessionName;
+
+while (passcode == null || passcode == '') {
+  passcode = prompt('Enter passcode', '');
+}
+meetingArgs.passcode = passcode;
+
+fetch('https://zlab.zoom.us/vsdk-auth', {
+  //fetch(`/api/`, {
+  method: 'POST',
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json'
+  },
+  body: JSON.stringify({
+    sessionName: meetingArgs.topic,
+    role: meetingArgs.role,
+    userIdentity: meetingArgs.userIdentity,
+    sessionKey: meetingArgs.sessionKey,
+    geoRegions: 'US,AU,CA,IN,CN,BR,MX,HK,SG,JP,DE,NL',
+    cloudRecordingOption: 1,
+    cloudRecordingElection: 0
+  })
+})
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    console.log('ajtha signature');
+    console.log(data.signature);
+    meetingArgs.signature = data.signature;
+    const zmClient = ZoomVideo.createClient();
+
+    ReactDOM.render(
+      <React.StrictMode>
+        <ZoomContext.Provider value={zmClient}>
+          <App meetingArgs={meetingArgs as any} />
+        </ZoomContext.Provider>
+      </React.StrictMode>,
+      document.getElementById('root')
+    );
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+console.log('=====================================');
+console.log('meetingArgs', meetingArgs);
+
+  const urlArgs: any = {
+    topic: meetingArgs.topic,
+    name: meetingArgs.name,
+    password: meetingArgs.password,
+    sessionKey: meetingArgs.sessionKey,
+    userIdentity: meetingArgs.userIdentity,
+    role: meetingArgs.role || 1,
+    cloud_recording_option: meetingArgs.cloud_recording_option || '',
+    cloud_recording_election: meetingArgs.cloud_recording_election || '',
+    telemetry_tracking_id: meetingArgs.telemetry_tracking_id || '',
+    enforceGalleryView: 0,
+    enforceVB: 0,
+    web: '1'
+  };
+  console.log('use url args');
+  console.log(window.location.origin + '/?' + new URLSearchParams(urlArgs).toString());
+}
+const zmClient = ZoomVideo.createClient();
+const root = createRoot(document.getElementById('root') as HTMLElement);
+root.render(
+  <React.StrictMode>
+    <ZoomContext.Provider value={zmClient}>
+      <App meetingArgs={meetingArgs as any} />
+    </ZoomContext.Provider>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
